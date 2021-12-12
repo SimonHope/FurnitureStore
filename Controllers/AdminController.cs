9162eb39cb725e63c92d8b689a43f26a2c0fb4e0
Nuttapong <devilkung52@gmail.com>
2021-12-12 14:36:30 +0700

    CRUD SYSTEM

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using FurnitureStore.Models;
 using FurnitureStore.Data;
 using System.Collections.Generic;
+using System;
 
 namespace FurnitureStore.Controllers
 {
@@ -17,28 +18,126 @@ namespace FurnitureStore.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Product()
+        public async Task<IActionResult> AdminProduct()
         {
             var contacts = await _context.Stocks.ToListAsync();
             return View(contacts);
         }
-
+       
         [HttpGet]
-        public async Task<IActionResult> Product(int id)
-        {
-            var exist = await _context.Stocks.Where(x => x.Id == id).FirstOrDefaultAsync();
-            return View(exist);
-        }
-
-        public IActionResult AdminProduct()
-        {
-            return View();
-        }
-
         public IActionResult AdminProductCreate()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AdminProductCreate(StockModel Stocks)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _context.Stocks.AddAsync(Stocks);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("AdminProduct");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+                }
+                
+            }
+
+            ModelState.AddModelError(string.Empty, "Something went wrong");
+
+            return View(Stocks);
+        }
+
+[HttpGet]
+public async Task<IActionResult> AdminProductEdit(int id)
+{
+    var exist = await _context.Stocks.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+    return View(exist);
+}
+
+[HttpPost]
+public async Task<IActionResult> AdminProductEdit(StockModel Stocks)
+{
+    // validate that our model meets the requirement
+    if (ModelState.IsValid)
+    {
+        try
+        {
+            // Check if the contact exist based on the id
+            var exist = _context.Stocks.Where(x => x.Id == Stocks.Id).FirstOrDefault();
+
+            // if the contact is not null we update the information
+            if(exist != null)
+            {
+                exist.Name = Stocks.Name;
+                exist.Desc = Stocks.Desc;
+                exist.Price = Stocks.Price;
+                exist.Disc = Stocks.Disc;
+                exist.Image = Stocks.Image;
+
+                // we save the changes into the db
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("AdminProduct");
+            }
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+        }
+    }
+
+    ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+    return View(Stocks);
+}
+
+[HttpGet]
+public async Task<IActionResult> AdminProductDelete(int id)
+{
+    var exist = await _context.Stocks.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+    return View(exist);
+}
+
+[HttpPost]
+public async Task<IActionResult> AdminProductDelete(StockModel Stocks)
+{
+    if (ModelState.IsValid)
+    {
+        try
+        {
+            var exist = _context.Stocks.Where(x => x.Id == Stocks.Id).FirstOrDefault();
+
+            if(exist != null)
+            {
+                _context.Remove(exist);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("AdminProduct");
+            } 
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError(string.Empty, $"Something went wrong {ex.Message}");
+        }
+    }
+
+    ModelState.AddModelError(string.Empty, $"Something went wrong, invalid model");
+
+    return View();
+}
+            
+            
+
+        
 
         public IActionResult AdminDashboard()
         {
