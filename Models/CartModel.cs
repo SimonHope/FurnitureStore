fd5f6ba87232cb972e516a4edaeb7da244b6f8a5
Aroon_Nose <57586253+Aroon134@users.noreply.github.com>
2021-12-13 00:02:02 +0700

    Add Controller and Model Cartshop

---
+++ Models/CartModel.cs
@@ -0,0 +1,10 @@
+namespace FurnitureStore.Models
+{
+    public class CartModel
+    {
+        
+        public int Id { get; set; }
+        public int StockId { get; set; }
+        public int QYN { get; set; }
+    }
+}
---
+++ Models/OrderModel.cs
@@ -0,0 +1,12 @@
+namespace FurnitureStore.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Address { get; set; }
+        public string IPAdr { get; set; }
+        public int CartId { get; set; }
+    }
+}
